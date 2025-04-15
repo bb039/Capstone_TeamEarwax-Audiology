8c7610bec138a9ebe115e773bcb61f5af21a7d8d
Tom Nguyen <snguyet773@gmail.com>
2025-04-15 16:35:38 -0500

    created keyboard

---
+++ AudiologyUnityProject/Assets/Scripts/KeyboardScript.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardScript : MonoBehaviour
+{
+    public InputField inputField;
+    public Text textField;
+
+    public void OnButtonPress()
+    {
+        inputField.text += textField.text;
+    }
+}
