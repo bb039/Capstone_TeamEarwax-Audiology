eef9c1bc46ac4de6e02ff17526c716181426fc05
JaysonGiri <sanjaygiriprakash@unomaha.edu>
2025-04-20 16:01:50 -0500

    added filters to sort the stats

--- AudiologyUnityProject/Assets/Scripts/DisplayStats.cs
+++ AudiologyUnityProject/Assets/Scripts/DisplayStats.cs
@@ -2,45 +2,93 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using UnityEngine.SceneManagement;
+using System;
+using System.Collections.Generic;
 
 public class StatsSceneScript : MonoBehaviour
 {
     public Transform contentParent;
     public GameObject statEntryRowPrefab;
-    private string filePath;
+    public InputField searchInput;
+    public Dropdown sortDropdown;
 
+    private string filePath;
     public string mainMenuScene;
+
+    private enum SortMode { LastCompleted, Fastest, Slowest }
+    private SortMode currentSortMode = SortMode.LastCompleted;
 
     void Start()
     {
         filePath = Application.persistentDataPath + "/stats.json";
+
+        if (sortDropdown != null)
+        {
+            sortDropdown.onValueChanged.RemoveAllListeners();
+            sortDropdown.onValueChanged.AddListener(OnSortOptionChanged);
+        }
+
         LoadStats();
     }
 
-    void LoadStats()
+    public void OnSearchButtonPressed()
     {
-        string path = Application.persistentDataPath + "/stats.json";
+        string input = searchInput.text;
+        LoadStats(input);
+    }
 
-        if (!File.Exists(path)) return;
+    void LoadStats(string nameFilter = "")
+    {
+        foreach (Transform child in contentParent)
+            Destroy(child.gameObject);
 
-        string json = File.ReadAllText(path);
+        if (!File.Exists(filePath)) return;
+
+        string json = File.ReadAllText(filePath);
         StatsData statsData = JsonUtility.FromJson<StatsData>(json);
+
+        List<(string name, float time)> combined = new List<(string, float)>();
 
         for (int i = 0; i < statsData.playerNames.Count; i++)
         {
-            GameObject row = Instantiate(statEntryRowPrefab, contentParent);
-
             string player = statsData.playerNames[i];
-            string time = FormatTime(statsData.times[i]);
+            float time = statsData.times[i];
 
-            Debug.Log($"Created entry: {player} - {time}");
+            if (!string.IsNullOrEmpty(nameFilter) &&
+                !player.ToLower().Contains(nameFilter.ToLower()))
+            {
+                continue;
+            }
 
+            combined.Add((player, time));
+        }
 
+        switch (currentSortMode)
+        {
+            case SortMode.Fastest:
+                combined.Sort((a, b) => a.time.CompareTo(b.time));
+                break;
+            case SortMode.Slowest:
+                combined.Sort((a, b) => b.time.CompareTo(a.time));
+                break;
+            case SortMode.LastCompleted:
+                break;
+        }
+
+        foreach (var (player, time) in combined)
+        {
+            GameObject row = Instantiate(statEntryRowPrefab, contentParent);
             row.transform.Find("PlayerNameText").GetComponent<Text>().text = player;
-            row.transform.Find("TimeText").GetComponent<Text>().text = time;
-
+            row.transform.Find("TimeText").GetComponent<Text>().text = FormatTime(time);
             row.SetActive(true);
         }
+    }
+
+    public void OnSortOptionChanged(int index)
+    {
+        Debug.Log("Dropdown changed: " + index);
+        currentSortMode = (SortMode)index;
+        LoadStats(searchInput.text);
     }
 
 
