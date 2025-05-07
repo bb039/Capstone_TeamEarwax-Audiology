d6d1d48edfb5ef5b67bc4c8efcbf9534fa0148c4
Tom Nguyen <snguyet773@gmail.com>
2025-05-07 17:31:11 -0500

    WIP spawner

--- AudiologyUnityProject/Assets/Scripts/HaplyScripts/CerumenCollisionHandler.cs
+++ AudiologyUnityProject/Assets/Scripts/HaplyScripts/CerumenCollisionHandler.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DespawnTrigger : MonoBehaviour
 {
+    private int value = 0;
+    [SerializeField] Text CerumenText;
+    [SerializeField] SpawnSphereScript spawnSphereScript;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<MovingSphere>() != null)
         {
+            value++;
             Destroy(other.gameObject);
+            CerumenText.text = $"Cerumen removed: {value}";
+            spawnSphereScript.Spawner(1);
         }
     }
 }
---
+++ AudiologyUnityProject/Assets/Scripts/SpawnSphereScript.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class SpawnSphereScript : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public Transform managerObject;
+    public int amountToSpawn = 4;
+
+    void Start()
+    {
+        Spawner(amountToSpawn);
+    }
+
+    public void Spawner(int amount)
+    {
+        float radius = managerObject.localScale.x * 0.5f - 0.001f;
+
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 spawnPos = new Vector3(0, 0, 0);
+            //int random = Random.Range(0, 2);
+            //if(random == 1)
+            //{
+            //    spawnPos = GetRandomEdgePosition(radius);
+            //}
+            //else
+            //{
+            //    spawnPos = GetRandomEdgePosition(radius);
+
+            //}
+
+            spawnPos = GetRandomEdgePosition(radius);
+            //Vector3 spawnPos = GetRandomPosition(radius);
+            Instantiate(spherePrefab, spawnPos, Quaternion.identity);
+        }
+    }
+
+    //Vector3 GetRandomBottomEdgePosition(float radius)
+    //{
+    //    float height = managerObject.localScale.y;
+    //    float angle = Random.Range(0f, 2f * Mathf.PI);
+    //    float x = Mathf.Cos(angle) * radius;
+    //    //float y = Mathf.Sin(angle) * radius;
+    //    float y = Random.Range(-height / 2, -height / 4);
+    //    //float z = Mathf.Sin(angle) * radius;
+    //    float z = Random.Range((float)(-managerObject.localScale.z + 0.2), (float)(managerObject.localScale.z - 0.2));
+    //    Vector3 localPos = new Vector3(x, y, z);
+
+    //    return managerObject.position + localPos;
+    //}
+
+    //Vector3 GetRandomTopEdgePosition(float radius)
+    //{
+    //    float height = managerObject.localScale.y;
+    //    float angle = Random.Range(0f, 2f * Mathf.PI);
+    //    float x = Mathf.Cos(angle) * radius;
+    //    //float y = Mathf.Sin(angle) * radius;
+    //    float y = Random.Range(height / 4, height/2);
+    //    //float z = Mathf.Sin(angle) * radius;
+    //    float z = Random.Range((float)(-managerObject.localScale.z + 0.2), (float)(managerObject.localScale.z - 0.2));
+    //    Vector3 localPos = new Vector3(x, y, z);
+
+    //    return managerObject.position + localPos;
+    //}
+
+    Vector3 GetRandomEdgePosition(float radius)
+    {
+        float height = managerObject.localScale.y;
+        float angle = Random.Range(0f, 2f * Mathf.PI);
+        float x = Mathf.Cos(angle) * radius;
+        float y = Mathf.Sin(angle) * radius;
+        //float y = Random.Range(height / 4, height / 2);
+        //float z = Mathf.Sin(angle) * radius;
+        float z = Random.Range((float)(-managerObject.localScale.z + 0.2), (float)(managerObject.localScale.z - 0.2));
+        Vector3 localPos = new Vector3(x, y, z);
+
+        return managerObject.position + localPos;
+    }
+
+    //Vector3 GetRandomPosition(float radius)
+    //{
+    //    float height = managerObject.localScale.y;
+
+    //    float angle = Random.Range(0f, Mathf.PI * 2);
+
+
+    //    float r = radius * Mathf.Sqrt(Random.value);
+    //    float x = r * Mathf.Cos(angle);
+
+    //    float z = r * Mathf.Sin(angle);
+    //    float y = Random.Range(-height, height);
+
+    //    Vector3 localPos = new Vector3(x, y, z);
+
+    //    return managerObject.TransformPoint(localPos);
+    //}
+}
